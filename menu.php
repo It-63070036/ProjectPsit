aff406271639eae9f47ecd3b00bc4cbde8f46901
ChutimaNakaew <73680207+ChutimaNakaew@users.noreply.github.com>
2020-12-13 00:43:36 +0700

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>Menu</title>
+</head>
+<body>
+    <from method="post">
+        Menu :
+        <p>Chicken</p>
+        <input type="checkbox" name='l[]' value="450"/>450 kcal
+        <p>Pizza</p>
+        <input type="checkbox" name='l[]' value="300"/>300 kcal
+        <br><br><input type="submit" value="Save">
+    </from>
+</body>
+</html>
