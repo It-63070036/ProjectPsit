c3f05fc28f121c41cb1b08d85fa62ac3ef06440d
ChutimaNakaew <73680207+ChutimaNakaew@users.noreply.github.com>
2020-12-13 15:53:19 +0700

    Add files via upload

---
+++ dbconnect.php
@@ -0,0 +1,5 @@
+ <?php
+
+ $con= mysqli_connect('ihost.it.kmitl.ac.th', 'it63070039_food', 'food1234', 'it63070039_food', 3306) or die("ไม่สามารถเชื่อมต่อฐานข้อมูลได้");
+ echo"เชื่อมต่อได้แล้วนะ";
+ ?>
